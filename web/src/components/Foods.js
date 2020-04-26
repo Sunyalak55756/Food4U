d8e63fb4205bd669b82737e558020c212a92f17e
Sunyalak <5735512057@email.psu.ac.th>
2020-04-26 22:41:12 +0700

    menu card

--- web/src/components/Foods.js
+++ web/src/components/Foods.js
@@ -11,18 +11,18 @@ const Foods = () => {
       <HeaderMenu />
       <Container text style={{ marginTop: '7em' }}>
         <Card.Group itemsPerRow={4}>
-          <Card color='red' image={src} />
-          <Card color='orange' image={src} />
-          <Card color='yellow' image={src} />
-          <Card color='olive' image={src} />
-          <Card color='green' image={src} />
-          <Card color='teal' image={src} />
-          <Card color='blue' image={src} />
-          <Card color='violet' image={src} />
-          <Card color='purple' image={src} />
-          <Card color='pink' image={src} />
-          <Card color='brown' image={src} />
-          <Card color='grey' image={src} />
+          <Card color='red' />
+          <Card color='orange' />
+          <Card color='yellow' />
+          <Card color='olive' />
+          <Card color='green' />
+          <Card color='teal' />
+          <Card color='blue' />
+          <Card color='violet' />
+          <Card color='purple' />
+          <Card color='pink' />
+          <Card color='brown' />
+          <Card color='grey' />
         </Card.Group>
       </Container>
     </div>
