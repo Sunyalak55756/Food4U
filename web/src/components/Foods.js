7400364f1606967e1cf71c74fa50b0b4c98a8092
Sunyalak <5735512057@email.psu.ac.th>
2020-04-26 21:59:48 +0700

    foods

--- web/src/components/Foods.js
+++ web/src/components/Foods.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import HeaderMenu from './HeaderMenu'
+import {
+  Container,
+  Header
+} from 'semantic-ui-react'
 
 const Foods = () => {
   return (
     <div>
-      Foods
+      <HeaderMenu />
+      <Container text style={{ marginTop: '7em' }}>
+        <Header as='h1'>Semantic UI React Fixed Template</Header>
+        <p>This is a basic fixed menu template using fixed size containers.</p>
+        <p>
+          A text container is used for the main container, which is useful for single column layouts.
+      </p>
+      </Container>
     </div>
   )
 }
