83ad2168e452a88ca98d7526c7a9cc27f90f67bb
Sunyalak <5735512057@email.psu.ac.th>
2020-04-29 21:25:44 +0700

    build

---
+++ web/build/precache-manifest.29ece8761f09868e21ac5daafdbd5179.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42b8f17e82cb41f2527d8d328dee578e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "268d2796d5d991277c18",
+    "url": "/static/css/2.7253a256.chunk.css"
+  },
+  {
+    "revision": "8368cbc680bc0de8b9c2",
+    "url": "/static/css/main.8df846ec.chunk.css"
+  },
+  {
+    "revision": "268d2796d5d991277c18",
+    "url": "/static/js/2.58bdeee4.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.58bdeee4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8368cbc680bc0de8b9c2",
+    "url": "/static/js/main.e17b8be0.chunk.js"
+  },
+  {
+    "revision": "668f4f542a4480b3c819",
+    "url": "/static/js/runtime-main.ff311fc1.js"
+  },
+  {
+    "revision": "7ac4178f5e714d52bbaf7633e61befef",
+    "url": "/static/media/2979c50de28b4137a3c0224591db4af5.7ac4178f.webp"
+  },
+  {
+    "revision": "dac997dc4adec428b87c25e8b5c71645",
+    "url": "/static/media/2fc2c60e210c4f53b9e454080b1bdab5.dac997dc.webp"
+  },
+  {
+    "revision": "f237224fbae82cb4dc9b5ad82adbca9c",
+    "url": "/static/media/6baaa3c4e37e4921b865e12586e16218.f237224f.webp"
+  },
+  {
+    "revision": "58fa8bbe8487646fd243cc92ac8c12e6",
+    "url": "/static/media/81948ff1597145cf967fa0b6c52caef3.58fa8bbe.jpg"
+  },
+  {
+    "revision": "5770e34521b5f9d97d5ad36c205722bb",
+    "url": "/static/media/88fec90d95bc47399efe3349b87bff54.5770e345.webp"
+  },
+  {
+    "revision": "e8bdac24870ddb6eaaaa1aa687e5d315",
+    "url": "/static/media/983145b5f634460592649f7c48132b54.e8bdac24.webp"
+  },
+  {
+    "revision": "6c2c5ccac8d590f0530e0d8ccfb7eb3b",
+    "url": "/static/media/a2d75934bc43465ea2b369efdc896bdb.6c2c5cca.webp"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "b0d5714db0b503fb30ea58632dc9257b",
+    "url": "/static/media/df2f09b24fe044a4b2633803565d7655.b0d5714d.webp"
+  },
+  {
+    "revision": "4da91c8da5a4ad05edf34fe54ea83835",
+    "url": "/static/media/e622fe4585c34e68aaebcdaafde39382.4da91c8d.webp"
+  },
+  {
+    "revision": "517324cc06da75472e10a2aa0ea93a58",
+    "url": "/static/media/ed3f343a5fd04a92a9b57137732b1fa7.517324cc.webp"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
